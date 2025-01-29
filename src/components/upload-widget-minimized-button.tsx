2a021d232555b300953ae175c88fd739db44e62b
Geovane Borba <geovanne@live.com>
2025-01-29 01:53:59 -0300

    feat: implement UploadWidgetMinimizedButton component

---
+++ src/components/upload-widget-minimized-button.tsx
@@ -0,0 +1,16 @@
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Maximize2 } from 'lucide-react'
+import { UploadWidgetTitle } from './upload-widget-title'
+
+export function UploadWidgetMinimizedButton() {
+  return (
+    <Collapsible.Trigger className="group flex w-full items-center justify-between gap-5 bg-white/2 px-5 py-3">
+      <UploadWidgetTitle />
+
+      <Maximize2
+        strokeWidth={1.5}
+        className="size-4 text-zinc-400 group-hover:text-zinc-100"
+      />
+    </Collapsible.Trigger>
+  )
+}
